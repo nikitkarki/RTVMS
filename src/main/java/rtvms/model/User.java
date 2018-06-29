e259da54bdf0ff60ce0055f044f23ac03e422894
nikitkarki <karki.niks@gmail.com>
2018-06-29 21:24:26 +0545

    user model added

---
+++ src/main/java/rtvms/model/User.java
@@ -0,0 +1,47 @@
+package rtvms.model;
+
+public class User {
+	private int userId;
+	private String firstName;
+	private String lastName;
+	private String dateOfBirth;
+	private String phoneNo;
+	private Address address;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+}
