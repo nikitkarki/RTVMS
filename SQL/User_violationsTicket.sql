4d534df5e3e0b8a609774e83b556e7afc362208a
nikitkarki <karki.niks@gmail.com>
2018-07-16 13:56:53 +0545

    a table to store ticket information added

---
+++ SQL/User_violationsTicket.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user_violations (
+violation_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+violationCharge INT(11) NOT NULL,
+violationDescription VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE user_ticket (
+ticket_id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+ticketNumber INT(11) NOT NULL,
+firstName VARCHAR(30) NOT NULL,
+lastName VARCHAR(30) NOT NULL,
+dateOfBirth VARCHAR(15) NOT NULL,
+issueDate VARCHAR(15) NOT NULL,
+licenseNumber VARCHAR(30) NOT NULL,
+FOREIGN KEY (violation) REFERENCES user_violations(violation_id)
+);
